7e38fadf3cbd5ab8b99111898e86aa6d8e20b677
Alan-Kuan <kuanhaoan@gmail.com>
2024-02-14 23:41:00 +0800

    feat: a moving ball

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <SFML/Graphics.hpp>
 
+const int kMsPerTick = 20;
+
 int main(void) {
     sf::RenderWindow window(sf::VideoMode(640, 480), "Pong Wars");
+    sf::Clock clock;
+    sf::CircleShape ball;
+
+    ball.setPosition(0, 0);
+    ball.setRadius(10);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -11,6 +18,15 @@ int main(void) {
                 window.close();
             }
         }
+
+        window.clear();
+
+        if (clock.getElapsedTime().asMilliseconds() % kMsPerTick == 0) {
+            ball.move(1, 1);
+        }
+
+        window.draw(ball);
+        window.display();
     }
 
     return 0;
