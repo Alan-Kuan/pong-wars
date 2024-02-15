8d3c9e6884862b2eca86bb538ec42473a99c63bf
Alan-Kuan <kuanhaoan@gmail.com>
2024-02-15 21:11:20 +0800

    feat: add blocks & style them

--- main.cpp
+++ main.cpp
@@ -1,26 +1,63 @@
+#include <vector>
+
 #include <SFML/Graphics.hpp>
 
-const unsigned int kWinWidth = 640;
-const int kMsPerTick = 20;
-const float kBoundRadius = 250;
+const unsigned int kWindowWidth = 640;
+const float kBoundaryWidth = 500;
+const float kBlockWidth = 20;
 const float kBallRadius = 10;
+const int kMsPerTick = 20;
+
+const struct {
+    sf::Color mystic_mint{217, 232, 227};
+    sf::Color nocturnal_expedition{17, 76, 90};
+    sf::Color ocean_noir{23, 43, 54};
+} kColorPalette;
 
 int main(void) {
-    sf::RenderWindow window(sf::VideoMode(kWinWidth, kWinWidth), "Pong Wars");
-    sf::View view(sf::Vector2f(0, 0), sf::Vector2f(kWinWidth, kWinWidth));
+    sf::RenderWindow window(sf::VideoMode(kWindowWidth, kWindowWidth), "Pong Wars");
+    sf::View view(sf::Vector2f(kBoundaryWidth / 2.0, kBoundaryWidth / 2.0), sf::Vector2f(kWindowWidth, kWindowWidth));
     window.setView(view);
 
-    sf::RectangleShape boundary;
-    boundary.setSize(sf::Vector2f(kBoundRadius * 2, kBoundRadius * 2));
-    boundary.setPosition(-kBoundRadius, -kBoundRadius);
-    boundary.setFillColor(sf::Color::Transparent);
-    boundary.setOutlineThickness(2);
-    boundary.setOutlineColor(sf::Color::White);
+    // background
+    sf::VertexArray bg(sf::Quads, 4);
+    float gap_width = (kWindowWidth - kBoundaryWidth) / 2.0;
+    bg[0].position = sf::Vector2f(-gap_width, -gap_width);
+    bg[1].position = sf::Vector2f(kBoundaryWidth + gap_width, -gap_width);
+    bg[2].position = sf::Vector2f(kBoundaryWidth + gap_width, kBoundaryWidth + gap_width);
+    bg[3].position = sf::Vector2f(-gap_width, kBoundaryWidth + gap_width);
+    bg[0].color = kColorPalette.ocean_noir;
+    bg[1].color = kColorPalette.ocean_noir;
+    bg[2].color = kColorPalette.mystic_mint;
+    bg[3].color = kColorPalette.mystic_mint;
 
+    // blocks
+    int block_num_per_dim = kBoundaryWidth / kBlockWidth;
+    std::vector<std::vector<sf::RectangleShape>> blocks(
+        block_num_per_dim,
+        std::vector<sf::RectangleShape>(block_num_per_dim)
+    );
+    for (int i = 0; i < block_num_per_dim; i++) {
+        for (int j = 0; j < block_num_per_dim; j++) {
+            auto& block = blocks[i][j];
+
+            block.setSize(sf::Vector2f(kBlockWidth, kBlockWidth));
+            block.setPosition(i * kBlockWidth, j * kBlockWidth);
+
+            if (i < block_num_per_dim / 2) {
+                block.setFillColor(kColorPalette.mystic_mint);
+            } else {
+                block.setFillColor(kColorPalette.nocturnal_expedition);
+            }
+        }
+    }
+
+    // ball
     sf::CircleShape ball;
-    ball.setPosition(100, -20);
+    ball.setPosition(350, 230);
     ball.setRadius(kBallRadius);
     ball.setOrigin(kBallRadius, kBallRadius);
+    ball.setFillColor(kColorPalette.mystic_mint);
 
     sf::Vector2f ball_vel(1, 1);
 
@@ -35,22 +72,57 @@ int main(void) {
             }
         }
 
-        window.clear();
-
-        if (clock.getElapsedTime().asMilliseconds() % kMsPerTick == 0) {
-            sf::Vector2f pos = ball.getPosition();
-
-            if (pos.x + kBallRadius > kBoundRadius || pos.x - kBallRadius < -kBoundRadius) {
-                ball_vel.x *= -1;
-            }
-            if (pos.y + kBallRadius > kBoundRadius || pos.y - kBallRadius < -kBoundRadius) {
-                ball_vel.y *= -1;
-            }
-
-            ball.move(ball_vel.x, ball_vel.y);
+        if (clock.getElapsedTime().asMilliseconds() % kMsPerTick != 0) {
+            continue;
         }
 
-        window.draw(boundary);
+        sf::Vector2f pos = ball.getPosition();
+        int i = pos.x / kBlockWidth;
+        int j = pos.y / kBlockWidth;
+        int left_side_i = (pos.x - kBallRadius) / kBlockWidth;
+        int right_side_i = (pos.x + kBallRadius) / kBlockWidth;
+        int top_side_j = (pos.y - kBallRadius) / kBlockWidth;
+        int bottom_side_j = (pos.y + kBallRadius) / kBlockWidth;
+
+        if (pos.x - kBallRadius < 0 || pos.x + kBallRadius >= kBoundaryWidth) {
+            ball_vel.x *= -1;
+        } else {
+            auto& left_block = blocks[left_side_i][j];
+            auto& right_block = blocks[right_side_i][j];
+
+            if (left_block.getFillColor() == kColorPalette.mystic_mint) {
+                left_block.setFillColor(kColorPalette.nocturnal_expedition);
+                ball_vel.x *= -1;
+            }
+            if (right_block.getFillColor() == kColorPalette.mystic_mint) {
+                right_block.setFillColor(kColorPalette.nocturnal_expedition);
+                ball_vel.x *= -1;
+            }
+        }
+        if (pos.y - kBallRadius < 0 || pos.y + kBallRadius >= kBoundaryWidth) {
+            ball_vel.y *= -1;
+        } else {
+            auto& top_block = blocks[i][top_side_j];
+            auto& bottom_block = blocks[i][bottom_side_j];
+
+            if (top_block.getFillColor() == kColorPalette.mystic_mint) {
+                top_block.setFillColor(kColorPalette.nocturnal_expedition);
+                ball_vel.y *= -1;
+            }
+            if (bottom_block.getFillColor() == kColorPalette.mystic_mint) {
+                bottom_block.setFillColor(kColorPalette.nocturnal_expedition);
+                ball_vel.y *= -1;
+            }
+        }
+
+        ball.move(ball_vel.x, ball_vel.y);
+
+        window.draw(bg);
+        for (int i = 0; i < block_num_per_dim; i++) {
+            for (int j = 0; j < block_num_per_dim; j++) {
+                window.draw(blocks[i][j]);
+            }
+        }
         window.draw(ball);
         window.display();
     }
